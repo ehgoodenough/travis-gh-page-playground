814db7078a22d4f9024ac758353235d46a937e0f
Andrew McPherson <andrewmcp333@gmail.com>
2015-03-10 22:32:09 -0400

    ugh not again

--- githubfile.js
+++ githubfile.js
@@ -1,4 +1,4 @@
-if(process.env.TRAVIS_PULL_REQUEST != "false") {
+/*if(process.env.TRAVIS_PULL_REQUEST != "false") {
     console.log("this is a pr")
 } else if(process.env.TRAVIS_BRANCH != "false") {
     if(process.env.TRAVIS_BRANCH == "master") {
@@ -6,4 +6,4 @@ if(process.env.TRAVIS_PULL_REQUEST != "false") {
     } else {
         console.log("this is any ole branch")
     }
-}
+}*/
