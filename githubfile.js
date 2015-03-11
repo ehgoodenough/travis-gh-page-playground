6a0ab1ece8a7ee7f677437191a3fbbd14076f3ad
Andrew McPherson <andrewmcp333@gmail.com>
2015-03-10 23:24:41 -0400

    phew

--- githubfile.js
+++ githubfile.js
@@ -7,3 +7,39 @@
         console.log("this is any ole branch")
     }
 }*/
+
+var async = require("async")
+var exec = require("child_process").exec
+var chalk = require("chalk")
+
+execute = function(cmd) {
+    return function(done) {
+        exec(cmd, function(jserr, stdout, stderr) {
+            console.log(chalk.bold("$ " + cmd))
+            if(jserr || stderr) {
+                console.log(jserr, stderr)
+            } else {
+                console.log(stdout)
+            }
+            if(done) {
+                done()
+            }
+        })
+    }
+}
+
+function run(commands) {
+    for(var index = 0; index < commands.length; index++) {
+        commands[index] = execute(commands[index])
+    }
+    async.series(commands, function(error, results) {
+        console.log("error", error)
+        console.log("results", results)
+    })
+}
+
+run([
+    "ls",
+    "cd " + process.cwd(),
+    "git status"
+])
